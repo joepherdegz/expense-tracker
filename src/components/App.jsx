e53ca16a1fa3683f1ef6c47bdbd3a12b2aa69492
joepherdegz <142654533+joepherdegz@users.noreply.github.com>
2024-08-27 20:33:20 -0700

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,15 @@ import React from "react";
 // import { Route, Routes } from "react-router-dom";
 import { Homepage } from "pages/HomePage";
 // import { SharedLayout } from "pages/SharedLayout";
+// import {WelcomePage} from "./WelcomePage/WelcomePage"
 
 export const App = () => {
     return (
         <>
             
             <Homepage />
-            {/* <Routes>
-                <Route path="/" element={<SharedLayout />}>
-                    <Route index element={<Homepage />}>
-
-                         
-                    </Route>
-                </Route>
-            </Routes> */}
+           
+             
         
         </>
          
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Logo from '../components/Header/Header';
-import BgImageWrapper from '../components/BgImageWrapper/BgImageWrapper'
+import { WelcomePage } from "../components/WelcomePage/WelcomePage";
+import styled from "styled-components";
 
+
+const Container = styled.div`
+// display: flex;
+// flex-direction: column;
+// width: 1440px;
+// height: 800px;
+// padding: 32px 100px 100px 100px;
+// font-size: 40px;
+color: white;
+`;
 export const Homepage = () => {
     return (
-        <div>
+        <>
             <Logo />
-            <BgImageWrapper/>
-        </div>
+        <Container>
+            
+            <WelcomePage/>
+        </Container>
+        </>
     );
 };
